6a536f6fd6d071bdd36cbfa887c5228367808bf3
Marcelo Tomazzini <marcelo_tomazzini@sicredi.com.br>
2025-04-08 21:28:55 -0300

    refatoração opçao 2

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -34,42 +34,13 @@ public class AdopetConsoleApplication {
                 if (opcaoEscolhida == 1) {
                     listarAbrigosCadastrados();
                 } else if (opcaoEscolhida == 2) {
-                    System.out.println("Digite o nome do abrigo:");
-                    String nome = new Scanner(System.in).nextLine();
-                    System.out.println("Digite o telefone do abrigo:");
-                    String telefone = new Scanner(System.in).nextLine();
-                    System.out.println("Digite o email do abrigo:");
-                    String email = new Scanner(System.in).nextLine();
-
-                    JsonObject json = new JsonObject();
-                    json.addProperty("nome", nome);
-                    json.addProperty("telefone", telefone);
-                    json.addProperty("email", email);
-
-                    HttpClient client = HttpClient.newHttpClient();
-                    String uri = "http://localhost:8080/abrigos";
-                    HttpRequest request = HttpRequest.newBuilder()
-                            .uri(URI.create(uri))
-                            .header("Content-Type", "application/json")
-                            .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
-                            .build();
-
-                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    int statusCode = response.statusCode();
-                    String responseBody = response.body();
-                    if (statusCode == 200) {
-                        System.out.println("Abrigo cadastrado com sucesso!");
-                        System.out.println(responseBody);
-                    } else if (statusCode == 400 || statusCode == 500) {
-                        System.out.println("Erro ao cadastrar o abrigo:");
-                        System.out.println(responseBody);
-                    }
+                    cadastrarNovoAbrigo();
                 } else if (opcaoEscolhida == 3) {
                     System.out.println("Digite o id ou nome do abrigo:");
                     String idOuNome = new Scanner(System.in).nextLine();
 
                     HttpClient client = HttpClient.newHttpClient();
-                    String uri = "http://localhost:8080/abrigos/" +idOuNome +"/pets";
+                    String uri = "http://localhost:8080/abrigos/" + idOuNome + "/pets";
                     HttpRequest request = HttpRequest.newBuilder()
                             .uri(URI.create(uri))
                             .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -90,7 +61,7 @@ public class AdopetConsoleApplication {
                         String nome = jsonObject.get("nome").getAsString();
                         String raca = jsonObject.get("raca").getAsString();
                         int idade = jsonObject.get("idade").getAsInt();
-                        System.out.println(id +" - " +tipo +" - " +nome +" - " +raca +" - " +idade +" ano(s)");
+                        System.out.println(id + " - " + tipo + " - " + nome + " - " + raca + " - " + idade + " ano(s)");
                     }
                 } else if (opcaoEscolhida == 4) {
                     System.out.println("Digite o id ou nome do abrigo:");
@@ -103,7 +74,7 @@ public class AdopetConsoleApplication {
                     try {
                         reader = new BufferedReader(new FileReader(nomeArquivo));
                     } catch (IOException e) {
-                        System.out.println("Erro ao carregar o arquivo: " +nomeArquivo);
+                        System.out.println("Erro ao carregar o arquivo: " + nomeArquivo);
                         continue;
                     }
                     String line;
@@ -175,7 +146,40 @@ public class AdopetConsoleApplication {
             JsonObject jsonObject = element.getAsJsonObject();
             long id = jsonObject.get("id").getAsLong();
             String nome = jsonObject.get("nome").getAsString();
-            System.out.println(id +" - " +nome);
+            System.out.println(id + " - " + nome);
+        }
+    }
+
+    private static void cadastrarNovoAbrigo() throws IOException, InterruptedException {
+        System.out.println("Digite o nome do abrigo:");
+        String nome = new Scanner(System.in).nextLine();
+        System.out.println("Digite o telefone do abrigo:");
+        String telefone = new Scanner(System.in).nextLine();
+        System.out.println("Digite o email do abrigo:");
+        String email = new Scanner(System.in).nextLine();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("nome", nome);
+        json.addProperty("telefone", telefone);
+        json.addProperty("email", email);
+
+        HttpClient client = HttpClient.newHttpClient();
+        String uri = "http://localhost:8080/abrigos";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+        String responseBody = response.body();
+        if (statusCode == 200) {
+            System.out.println("Abrigo cadastrado com sucesso!");
+            System.out.println(responseBody);
+        } else if (statusCode == 400 || statusCode == 500) {
+            System.out.println("Erro ao cadastrar o abrigo:");
+            System.out.println(responseBody);
         }
     }
 }
