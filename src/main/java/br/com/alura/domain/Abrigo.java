800351f6fb6b00b75df91522a2c990975537e39f
Marcelo Tomazzini <marcelo_tomazzini@sicredi.com.br>
2025-04-14 20:44:33 -0300

    bugfix cadastrar abrigo

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,17 +2,24 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    public Abrigo() {
+
+    }
+
+    public Abrigo(String nome, String endereco, String email, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
     private Long id;
     private String nome;
-    private String telefone;
+    private String endereco;
     private String email;
+    private String telefone;
 
-    // construtor
-    public Abrigo(String nome, String telefone, String email) {
-        this.nome = nome;
-        this.telefone = telefone;
-        this.email = email;
-    }
+    private Pet[] pets;
 
     public Long getId() {
         return id;
@@ -20,5 +27,37 @@ public class Abrigo {
 
     public String getNome() {
         return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 }
